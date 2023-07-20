bf8515c3d63e49de26d029e7c23b6d8d214fef17
Alberto Vargas <albertovargas@intelligo.mx>
2023-07-19 22:33:02 -0600

    feat: :sparkles: adds bootstrap-table

--- read_db.php
+++ read_db.php
@@ -30,40 +30,67 @@ if ($result = $conn->query($sql)) {
 }
 $conn->close();
 ?>
-
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.22.1/dist/bootstrap-table.min.css">
+  </head>
 <body>
-    <table cellspacing="5" cellpadding="5">
-        <tr>
-            <td>ID</td>
-            <td>Sensor</td>
-            <td>Temperatura</td>
-            <td>Ritmo Cardiaco</td>
-            <td>Saturacion de Oxigeno</td>
-            <td>Marca de Tiempo</td>
-        </tr>
-        <?php foreach ($rows as $index => $result_row): ?>
-            <tr>
-                <td>
-                    <?= $result_row['id']; ?>
-                </td>
-                <td>
-                    <?= $result_row['sensor_names']; ?>
-                </td>
-                <td>
-                    <?= $result_row['temperature_value']; ?>
-                </td>
-                <td>
-                    <?= $result_row['heart_rate_value']; ?>
-                </td>
-                <td>
-                    <?= $result_row['oxygen_saturation_value']; ?>
-                </td>
-                <td>
-                    <?= $result_row['reading_time']; ?>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
+    <div class="container">
+        <table
+        data-toggle="table"
+        data-show-columns="true"
+        data-show-export="true"
+        data-height="460"
+        data-pagination="true">
+            <thead>
+                <tr class="tr-class-1">
+                    <th data-field="id" data-valign="middle">Id</th>
+                    <th data-field="sensor-name" data-custom-attribute="star">Sensor</th>
+                    <th data-field="temperature" data-custom-attribute="forks">Temperatura</th>
+                    <th data-field="heart-rate">Ritmo Cardiaco</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr id="tr-id-1" class="tr-class-1" data-title="bootstrap table" data-object='{"key": "value"}'>
+                    <td id="td-id-1" class="td-class-1" data-title="bootstrap table">
+                        <?= $rows[0]['id']; ?>
+                    </td>
+                    <td data-value="526">
+                        <?= $rows[0]['sensor_names']; ?>
+                    </td>
+                    <td data-text="122">
+                        <?= $rows[0]['temperature_value']; ?>
+                    </td>
+                    <td data-i18n="Description">
+                        <?= $rows[0]['heart_rate_value']; ?>
+                    </td>
+                </tr>
+                <?php foreach ($rows as $index => $result_row): ?>
+                <tr id="tr-id-2" class="tr-class-2">
+                    <td id="td-id-2" class="td-class-2">
+                        <?= $result_row['id']; ?>
+                    </td>
+                    <td>
+                        <?= $result_row['sensor_names']; ?>
+                    </td>
+                    <td>
+                        <?= $result_row['temperature_value']; ?>
+                    </td>
+                    <td>
+                        <?= $result_row['heart_rate_value']; ?>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/tableexport.jquery.plugin/tableExport.min.js"></script>
+    <script src="https://unpkg.com/bootstrap-table@1.22.1/dist/bootstrap-table.min.js"></script>
+    <script src="https://unpkg.com/bootstrap-table@1.22.1/dist/extensions/export/bootstrap-table-export.min.js"></script>
 </body>
-
 </html>
